801917b50f8b926fd71daf24fe9b729ffebdefd2
Giovani <giovani@teste.com>
2025-06-25 21:14:16 -0300

    com erro no hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imageMin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ style: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('./dist/images'));
+}
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.parallel(images));
 }
